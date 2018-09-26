b15ca5453b95a71b36e4fb5827da1b76c19ea614
junzhe1993 <31632630+junzhe1993@users.noreply.github.com>
2018-09-26 13:38:05 -0400

    Add files via upload

---
+++ tests/test_US03.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/9/22
+# @Author  : Zhe Jun
+# @File    : US03.py
+# @Software: PyCharm
+import unittest
+from US03 import birt_before_deat
+
+
+class TestBirtBeforeDeat(unittest.TestCase):
+
+    def test_birt_before_deat(self):
+        self.assertRaises(Exception, birt_before_deat, [{'BIRT': '1990-09-12', 'DEAT': '1989-01-15'}])
+        self.assertRaises(Exception, birt_before_deat, [{'BIRT': '1990-09-12', 'DEAT': '1989-01-15'}])
+        self.assertRaises(Exception, birt_before_deat, [{'BIRT': '1990-09-12', 'DEAT': '1989-01-15'}])
+        self.assertEqual(birt_before_deat([{'BIRT': '1990-09-12', 'DEAT': '1991-01-15'}]), None)
+        self.assertEqual(birt_before_deat([{'BIRT': '1990-09-12', 'DEAT': '1991-01-15'}]), None)
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False, verbosity=2)
---
+++ tests/test_US05.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/9/23
+# @Author  : Zhe Jun
+# @File    : test_US05.py
+# @Software: PyCharm
+import unittest
+from US05 import marr_before_deat
+
+
+class TestMarrBeforeDeat(unittest.TestCase):
+
+    def test_marr_before_deat(self):
+        self.assertRaises(Exception, marr_before_deat, [{'DEAT': '1992-08-15', 'SPOUSE': ['@F1@']}],
+                          [{'MARR': '1999-06-15'}])
+        self.assertRaises(Exception, marr_before_deat, [{'DEAT': '1999-08-1', 'SPOUSE': ['@F1@']}],
+                          [{'MARR': '2003-08-20'}])
+        self.assertRaises(Exception, marr_before_deat, [{'DEAT': '2010-08-25', 'SPOUSE': ['@F1@', '@F2@']}],
+                          [{'MARR': '2012-08-15'}, {'MARR': '2018-08-17'}])
+        self.assertEqual(marr_before_deat([{'DEAT': '1994-08-15', 'SPOUSE': ['@F1@']}], [{'MARR': '1968-12-15'}]), None)
+        self.assertEqual(marr_before_deat([{'DEAT': '1994-08-15', 'SPOUSE': 'NONE'}], [{'MARR': 'NONE'}]), None)
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False, verbosity=2)
