ce8338e40483c9a09cc52d7a0813337c0631a6c5
meowangS <wjxsuper1@126.com>
2018-10-02 19:51:58 -0400

    bug fixed

--- UserStories/US11.py
+++ UserStories/US11.py
@@ -11,12 +11,13 @@ def no_bigamy (indilist,famlist):
     marr = []
     div = []
     for people in indilist:
+        print(people)
         if len(people['SPOUSE'])>=2 and people['SPOUSE']!='NONE':
             for spouse in people['SPOUSE']:
-                #print(spouse)
+                print(spouse)
                 find_fam_index = int(spouse[2])-1
-                #print(find_fam_index)
-                if famlist[find_fam_index]['DIV']== 'NONE':
+                print(find_fam_index)
+                if famlist[find_fam_index]['DIV'] == 'NONE':
                     tem = int((datetime.now() - datetime.strptime(famlist[find_fam_index]['MARR'], '%Y-%m-%d')).days)
                     marr.append(tem)
                     div.append(1000000)
