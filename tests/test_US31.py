638cc11103d75f974f656438cecee35f2752becd
yang12304 <31525847+yang12304@users.noreply.github.com>
2018-10-25 16:17:10 -0400

    Add files via upload

---
+++ tests/test_US31.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : test_US31.py
+# @Author: Zhiren Yang
+# @Date  : 18-10-19
+# @Software : PyCharm
+# @Desc  : test--List all living people over 30 who have never been married in a GEDCOM file
+
+import unittest
+from UserStories.US31 import list_living_single
+
+
+class TestListLivingSingle(unittest.TestCase):
+
+    def test_list_living_single(self):
+
+        l1 = [{'num': 3, 'CHIL': 'NONE', 'AGE': 24, 'NAME': 'Kaiwen /Xue/', 'SEX': 'M', 'ALIVE': 'True', 'DEAT': 'NA',
+              'INDI': '@I1@', 'SPOUSE': 'NONE', 'BIRT': '1994-08-15'},
+             {'num': 10, 'CHIL': ['@I1@'], 'DEAT': 'NA', 'AGE': 60, 'NAME': 'Mingxuan /Xue/', 'SEX': 'M', 'ALIVE': 'True',
+              'SPOUSE': ['@F1@'], 'INDI': '@I2@', 'BIRT': '1994-08-15'},
+             {'num': 18, 'CHIL': ['@I1@'], 'DEAT': 'NA', 'AGE': 54, 'NAME': 'Huifang /Li/', 'SEX': 'F', 'ALIVE': 'True',
+              'SPOUSE': ['@F1@'], 'INDI': '@I3@', 'BIRT': '1964-09-15'},
+             {'num': 25, 'CHIL': ['@I2@', '@I6@'], 'AGE': 50, 'DEAT': '1984-08-23', 'SEX': 'M', 'ALIVE': 'False',
+              'SPOUSE': ['@F2@'], 'BIRT': '1934-04-12', 'INDI': '@I4@', 'NAME': 'Zhishan /Xue/'},
+             {'num': 34, 'CHIL': ['@I2@', '@I6@'], 'AGE': 46, 'DEAT': '1984-08-06', 'SEX': 'F', 'ALIVE': 'False',
+              'SPOUSE': ['@F2@'], 'BIRT': '1938-09-17', 'INDI': '@I5@', 'NAME': 'Xiuzhen /Mei/'},
+             {'num': 43, 'CHIL': ['@I9@'], 'DEAT': 'NA', 'AGE': 60, 'NAME': 'Xiumei /Xue/', 'SEX': 'F', 'ALIVE': 'True',
+              'SPOUSE': ['@F3@', '@F4@'], 'INDI': '@I6@', 'BIRT': '1994-08-15'},
+             {'num': 52, 'CHIL': ['@I9@'], 'AGE': 28, 'DEAT': '1985-11-12', 'SEX': 'M', 'ALIVE': 'False', 'SPOUSE': ['@F4@'],
+              'BIRT': '1957-10-07', 'INDI': '@I7@', 'NAME': 'Zhenli /Zhang/'},
+             {'num': 61, 'CHIL': ['@I10@'], 'DEAT': 'NA', 'AGE': 66, 'NAME': 'Jianjun /Dang/', 'SEX': 'M', 'ALIVE': 'True',
+              'SPOUSE': ['@F3@'], 'INDI': '@I8@', 'BIRT': '1994-08-15'},
+             {'num': 68, 'CHIL': 'NONE', 'AGE': 34, 'NAME': 'Jianguo /Zhang/', 'SEX': 'M', 'ALIVE': 'True', 'DEAT': 'NA',
+              'INDI': '@I9@', 'SPOUSE': 'NONE', 'BIRT': '1994-08-15'},
+             {'num': 75, 'CHIL': 'NONE', 'AGE': 29, 'NAME': 'Weimin /Dang/', 'SEX': 'M', 'ALIVE': 'True', 'DEAT': 'NA',
+              'INDI': '@I10@', 'SPOUSE': 'NONE', 'BIRT': '1994-08-15'}]
+
+        self.assertIs(list_living_single(l1[:7]), None)
+        self.assertIs(list_living_single(l1[:9]), None)
+        self.assertIs(list_living_single(l1), None)
+        self.assertEqual(list_living_single(l1), None)
+        self.assertIs(list_living_single(l1[3:5]), None)
+
+
+if __name__ == '__main__':
+    unittest.main()
