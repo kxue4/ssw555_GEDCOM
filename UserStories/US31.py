f9958d345c6a7d87c468f72a04e4da8a9a44a879
yang12304 <31525847+yang12304@users.noreply.github.com>
2018-10-25 16:16:43 -0400

    Add files via upload

---
+++ UserStories/US31.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : US31.py
+# @Author: Zhiren Yang
+# @Date  : 18-10-17
+# @Software : PyCharm
+# @Desc  : List all living people over 30 who have never been married in a GEDCOM file
+
+
+def list_living_single(indi_list):
+    ls = []
+    for i in indi_list:
+        if i['AGE'] > 30 and i['SPOUSE'] == 'NONE':
+            ls.append(i['INDI'] + '_lineNum:' + str(i['num']))
+    print("INFO :" + " All living people over 30 who have never been married are: " + str(ls))
